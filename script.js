6acb90e57757f229f9b49e7a3aeb722fc5157dcf
alan <alan.silva.cr41@gmail.com>
2024-08-23 14:18:10 -0300

    projeto 0.1

---
+++ script.js
@@ -0,0 +1,11 @@
+function search() {
+    const query = document.getElementById('search').value;
+    const searchResults = document.getElementById('search-results');
+  
+    // Simples exemplo de busca
+    if (query) {
+      searchResults.innerHTML = `<p>Não temos isso no momento :( </p>`;
+    } else {
+      searchResults.innerHTML = `<p>Por favor, insira um termo de pesquisa.</p>`;
+    }
+  }
