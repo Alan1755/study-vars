0b251d7faa8dd6bfec0ace468b136895977eb7bf
alan <alan.silva.cr41@gmail.com>
2024-08-23 15:50:22 -0300

    0.2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function search() {
-    const query = document.getElementById('search').value;
-    const searchResults = document.getElementById('search-results');
-  
-    // Simples exemplo de busca
-    if (query) {
-      searchResults.innerHTML = `<p>Não temos isso no momento :( </p>`;
-    } else {
-      searchResults.innerHTML = `<p>Por favor, insira um termo de pesquisa.</p>`;
-    }
+  const query = document.getElementById('search').value;
+  const searchResults = document.getElementById('search-results');
+
+  // Simples exemplo de busca
+  if (query) {
+    searchResults.innerHTML = `<p>Não temos isso no momento :( </p>`;
+  } else {
+    searchResults.innerHTML = `<p>Por favor, insira um termo de pesquisa.</p>`;
   }
+}
